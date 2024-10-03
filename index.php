a8cd499b6b0ef31007bcf23ec6d3b1d63788c81e
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-03 16:28:02 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>php-hotel</title>
+
+</html>
+
+<body>
+
+</body>
+
+</html>
